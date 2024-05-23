57bdb21f1b527c25bbefc0e04c8f636c11a1ffdc
vijayakandula <kandulavijaya1900@gmail.com>
2024-05-23 14:37:19 +0530

    new println

--- src/Firstprogram.java
+++ src/Firstprogram.java
@@ -3,6 +3,7 @@ public class Firstprogram {
     public static void main (String[] args )
     {
         System.out.println("hello intellij");
+        System.out.println("my first program");
     }
 
 }
